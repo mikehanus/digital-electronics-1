4c6edc76a0ca94fad65b90b1e265bb9ee3bfd3ad
Hanus Michal (240621) <240621@vutbr.cz>
2023-03-29 10:20:45 +0200

    08 project, 07 repport

---
+++ 08-traffic_lights/traffic/traffic.srcs/sim_1/new/nexys-a7-50t.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 29.03.2023 09:37:12
+-- Design Name: 
+-- Module Name: nexys-a7-50t - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity nexys-a7-50t is
+--  Port ( );
+end nexys-a7-50t;
+
+architecture Behavioral of nexys-a7-50t is
+
+begin
+
+
+end Behavioral;
